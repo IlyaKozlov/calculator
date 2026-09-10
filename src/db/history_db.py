082b357e93ded22ad93e21277bcaca833c873a20
juliaowl7-lang <julia.owl.7@gmail.com>
2026-09-10 15:28:02 +0200

    update history_db.py

--- src/db/history_db.py
+++ src/db/history_db.py
@@ -1,6 +1,7 @@
 import json
 from pathlib import Path
 from time import time
+import sqlite3
 
 from db.abstract_db import AbstractDb
 
@@ -9,19 +10,65 @@ class HistoryDb(AbstractDb):
 
     _path = Path(__file__).parent / "History.db"
 
+    def __init__(self):
+        self._create_table()
+
+    def _get_connection(self):
+        return sqlite3.connect(self._path)
+
+    def _create_table(self):
+        connection = self._get_connection()
+        cursor = connection.cursor()
+
+        cursor.execute("""
+                CREATE TABLE IF NOT EXISTS calculations (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    first_number REAL NOT NULL,
+                    operation TEXT NOT NULL,
+                    second_number REAL NOT NULL,
+                    timestamp REAL NOT NULL
+                )
+            """)
+
+        connection.commit()
+        connection.close()
+
     def history_save(self, first_number: float, operation: str, second_number: float):
-        with open(self._path, "a") as db:
-            item = {"first_number": first_number,
-                    "operation": operation,
-                    "second_number": second_number,
-                    "timestamp": time()
-                    }
-            print(f"add new operation to db, {item}")
-            db.write(json.dumps(item) + "\n")
+        connection = self._get_connection()
+        cursor = connection.cursor()
+
+        cursor.execute(
+            """
+            INSERT INTO calculations (first_number, operation, second_number, timestamp)
+            VALUES (?, ?, ?, ?)
+            """,
+            (first_number, operation, second_number, time())
+        )
+
+        connection.commit()
+        connection.close()
 
     def history_load(self) -> list[dict]:
-        if  not self._path.exists():
-            return []
-        with open(self._path, "r") as db:
-            return [json.loads(line) for line in db]
+        connection = self._get_connection()
+        cursor = connection.cursor()
+
+        cursor.execute("""
+                SELECT first_number, operation, second_number, timestamp
+                FROM calculations
+                ORDER BY id
+            """)
+
+        rows = cursor.fetchall()
+        connection.close()
+
+        return [
+            {
+                "first_number": row[0],
+                "operation": row[1],
+                "second_number": row[2],
+                "timestamp": row[3]
+            }
+            for row in rows
+        ]
+
 
